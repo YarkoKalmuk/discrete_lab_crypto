7def52bafec0058548ffb2de7c721f4484f74236
Kalmuk Yaropolk <yarko.kalmuk@gmail.com>
2025-04-13 21:32:55 +0300

    Server and client private, public key generation and exchange done

--- discrete_lab2-master/client.py
+++ discrete_lab2-master/client.py
@@ -1,11 +1,19 @@
 import socket
 import threading
+import random
+import math
 
 class Client:
     def __init__(self, server_ip: str, port: int, username: str) -> None:
         self.server_ip = server_ip
         self.port = port
         self.username = username
+        self.p = None
+        self.q = None
+        self.n = None
+        self.e = None
+        self.phi_n = None
+        self.d = None
 
     def init_connection(self):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -18,37 +26,84 @@ class Client:
         self.s.send(self.username.encode())
 
         # create key pairs
+        self.p, self.q = self.generate_prime(1000, 5000), self.generate_prime(1000, 5000) # private p, q
+        while self.p==self.q:
+            self.q = self.generate_prime(1000, 5000)
+
+        self.n = self.p * self.q # public key "n"
+        self.phi_n = (self.p-1)*(self.q-1)
+        self.e =random.randint(3, self.phi_n-1) # public key "e"
+        while math.gcd(self.e, self.phi_n) !=1 :
+            self.e =random.randint(3, self.phi_n-1)
+
+        self.d = self.mod_inverse(self.e, self.phi_n) # secret key "d"
 
         # exchange public keys
+        public_key = f"{self.n},{self.e}"  # Sending n and e as a public key
+        self.s.send(public_key.encode())
 
         # receive the encrypted secret key
+        server_public_key = self.s.recv(1024).decode()
+        print(f"Received server public key: {server_public_key}")
 
         message_handler = threading.Thread(target=self.read_handler,args=())
         message_handler.start()
         input_handler = threading.Thread(target=self.write_handler,args=())
         input_handler.start()
 
-    def read_handler(self): 
+    def read_handler(self):
+        """Reads encryptde message"""
         while True:
             message = self.s.recv(1024).decode()
 
             # decrypt message with the secrete key
-
-            # ... 
-
+            encrypted_numbers = list(map(int, message.split(",")))
+            message_encoded = [pow(ch, self.d, self.n) for ch in encrypted_numbers]
+            message = "".join(chr(ch) for ch in message_encoded)
 
             print(message)
 
     def write_handler(self):
+        """Writes a message and encrypts it"""
         while True:
             message = input()
 
             # encrypt message with the secrete key
+            message_encoded = [ord(ch) for ch in message]
 
-            # ...
+            # (m^e)modn = c
+            ciphered_text = [pow(ch, self.e, self.n) for ch in message_encoded]
+            encrypted_message = ",".join(map(str, ciphered_text))
 
-            self.s.send(message.encode())
+            self.s.send(encrypted_message.encode())
+
+    @staticmethod
+    def is_prime(number) -> bool:
+        """Checks if a number is prime"""
+        if number <2:
+            return False
+
+        for n in range(2, int(math.sqrt(number)+1)):
+            if number % n == 0:
+                return False
+        return True
+
+    @staticmethod
+    def generate_prime(min_value, max_value) -> int:
+        """Generates a random prime number in given range"""
+        prime = random.randint(min_value, max_value)
+        while not Client.is_prime(prime):
+            prime = random.randint(min_value, max_value)
+        return prime
+
+    @staticmethod
+    def mod_inverse(e, phi) -> int:
+        """Finds an inversed num to e with module phi"""
+        for d in range(3, phi):
+            if (d*e)%phi == 1:
+                return d
+        raise ValueError("gcd(e, d) is not 1")
 
 if __name__ == "__main__":
-    cl = Client("127.0.0.1", 9001, "b_g")
+    cl = Client("127.0.0.1", 9001, "yaropolk 1")
     cl.init_connection()
--- discrete_lab2-master/server.py
+++ discrete_lab2-master/server.py
@@ -1,6 +1,7 @@
 import socket
 import threading
-
+import random
+import math
 class Server:
 
     def __init__(self, port: int) -> None:
@@ -9,12 +10,21 @@ class Server:
         self.clients = []
         self.username_lookup = {}
         self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.public_keys = {}
 
     def start(self):
         self.s.bind((self.host, self.port))
         self.s.listen(100)
 
         # generate keys ...
+        server_p, server_q = self.generate_prime(1000, 5000), self.generate_prime(1000, 5000)
+        server_n = server_p * server_q
+        server_phi_n = (server_p - 1) * (server_q - 1)
+        server_e = random.randint(3, server_phi_n - 1)
+        while math.gcd(server_e, server_phi_n) != 1:
+            server_e = random.randint(3, server_phi_n - 1)
+        server_public_key = f"{server_n},{server_e}"
+
 
         while True:
             c, addr = self.s.accept()
@@ -24,7 +34,14 @@ class Server:
             self.username_lookup[c] = username
             self.clients.append(c)
 
-            # send public key to the client 
+            # receive client`s public key
+            client_public_key = c.recv(1024).decode()
+            print(f"Received client public key: {client_public_key}")
+            self.public_keys[c] = client_public_key
+
+            # print(self.clients, self.public_keys)
+            # send public key to the client
+            c.send(server_public_key.encode())
 
             # ...
 
@@ -32,28 +49,67 @@ class Server:
 
             # ...
 
-            # send the encrypted secret to a client 
+            # send the encrypted secret to a client
 
             # ...
 
             threading.Thread(target=self.handle_client,args=(c,addr,)).start()
 
     def broadcast(self, msg: str):
-        for client in self.clients: 
+        for client in self.clients:
 
             # encrypt the message
 
-            # ...
+            # Отримати публічний ключ клієнта
+            pub_key = self.public_keys[client]
+            n, e = map(int, pub_key.split(","))
 
-            client.send(msg.encode())
+            # Зашифрувати повідомлення для цього клієнта
+            encrypted_msg = [pow(ord(ch), e, n) for ch in msg]
 
-    def handle_client(self, c: socket, addr): 
+            # Перетворити у байти і надіслати
+            client.send(",".join(map(str, encrypted_msg)).encode())
+
+    def handle_client(self, c: socket, addr):
         while True:
             msg = c.recv(1024)
 
             for client in self.clients:
                 if client != c:
                     client.send(msg)
+
+
+    @staticmethod
+    def generate_prime(min_value, max_value) -> int:
+        """Generates a random prime number in given range"""
+        prime = random.randint(min_value, max_value)
+        while not Server.is_prime(prime):
+            prime = random.randint(min_value, max_value)
+        return prime
+
+
+    @staticmethod
+    def is_prime(number) -> bool:
+        """Checks if a number is prime"""
+        if number <2:
+            return False
+
+        for n in range(2, int(math.sqrt(number)+1)):
+            if number % n == 0:
+                return False
+        return True
+
+
+
+    @staticmethod
+    def mod_inverse(e, phi) -> int:
+        """Finds an inversed num to e with module phi"""
+        for d in range(3, phi):
+            if (d*e)%phi == 1:
+                return d
+        raise ValueError("gcd(e, d) is not 1")
+
+
 
 if __name__ == "__main__":
     s = Server(9001)
